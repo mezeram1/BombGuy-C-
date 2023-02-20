c392ceb18a4f1c5f4fbce899bc9e6c33d23e3f38
Matěj Mezera <114945228+mezeram1@users.noreply.github.com>
2023-02-20 21:04:53 +0100

    Add files via upload

---
+++ C# - projekty/Sokoban/Program.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sokoban
+{
+    class Program
+    {
+        static int[,] VyrobSkladiste()
+        {
+            int[,] skladiste =new int[12,12];
+            for (int i = 0; i < 12; i++)
+            {
+                skladiste[0,i] = -1;
+                skladiste[11,i] = -1;
+                skladiste[i,0] = -1;
+                skladiste[i,11] = -1;
+            }
+            return skladiste;
+        }
+        static List<(int, int)> VyplnSkladiste(int[,] skladiste)
+        {
+            List<(int, int)> info = new List<(int, int)>();
+            (int,int) skladnik = (0,0);
+            (int,int) bedna = (0,0);
+            (int,int) cil = (0,0);
+            for (int i = 1; i < 11; i++)
+            {
+                string radek = Console.ReadLine();
+                for (int j = 0; j < 10; j++)
+                {
+                    if (radek[j] == 'S')
+                    {
+                        skladnik = (i,j+1);
+                    }
+                    else if (radek[j] == 'B')
+                    {
+                        bedna = (i,j+1);
+                    }
+                    else if (radek[j] == 'C')
+                    {
+                        skladiste[i,j+1] = 'C';
+                        cil = (i,j+1);
+                    }
+                    else if (radek[j] == 'X')
+                    {
+                        skladiste[i, j+1] = -1;
+                    }
+                }
+            }
+            info.Add(skladnik);
+            info.Add(bedna);
+            info.Add(cil);
+            return info;
+        }
+        static void VypisSkladiste(int[,] skladiste, (int,int) skladnik, (int,int) bedna)
+        {
+            for (int i = 0; i < 12; i++)
+            {
+                for (int j = 0; j < 12; j++)
+                {
+                    if ((i,j) == skladnik)
+                        Console.Write("S ");
+                    else if ((i,j) == bedna)
+                        Console.Write("B ");
+                    else if (skladiste[i,j] == -1)
+                        Console.Write("1 ");
+                    else                    
+                       Console.Write("{0} ", skladiste[i,j]); 
+                }
+                Console.WriteLine();
+            }
+        }
+        static bool JsemVohu(int[,] skladiste, (int, int) bedna)
+        {
+            if (skladiste[bedna.Item1 + 1,bedna.Item2] == -1 || skladiste[bedna.Item1 - 1,bedna.Item2] == -1)
+            {
+                if (skladiste[bedna.Item1,bedna.Item2 + 1] == -1 || skladiste[bedna.Item1,bedna.Item2 - 1] == -1)
+                    return true;
+                else
+                    return false;
+            }
+            else
+                return false;
+        }
+        static int MuzuTahnout(int[,] skladiste, (int,int) skladnik, (int,int) bedna, (int,int) prirustek, (int,int) cil)
+        {
+            if (skladiste[skladnik.Item1 + prirustek.Item1,skladnik.Item2 + prirustek.Item2] != -1)
+            {
+                if ((skladnik.Item1 + prirustek.Item1,skladnik.Item2 + prirustek.Item2) == bedna)
+                {
+                    if (skladiste[bedna.Item1 + prirustek.Item1, bedna.Item2 + prirustek.Item2] != -1)
+                    {
+                        if (skladnik.Item1 == bedna.Item1)
+                        {
+                            if (! JsemVohu(skladiste, (bedna.Item1, bedna.Item2 + prirustek.Item2)) || (bedna.Item1, bedna.Item2 + prirustek.Item2) ==  cil)
+                                return 1;
+                            else
+                                return -1;
+                        }
+                        else
+                        {
+                            if (! JsemVohu(skladiste, (bedna.Item1 + prirustek.Item1, bedna.Item2)) || (bedna.Item1 + prirustek.Item1, bedna.Item2) ==  cil)
+                                return 1;
+                            else
+                                return -1;
+                        }
+                    }
+                    else
+                        return -1;
+                }
+                else
+                    return 0;
+            }
+            else
+                return -1;
+        }
+        static void Hledej(int[,] skladiste, (int, int) skladnik, (int, int) bedna, (int, int) cil)
+        {
+            ((int,int),(int,int),int)[] fronta = new ((int, int), (int, int), int)[10000];
+            Dictionary<((int,int),(int,int)),int> vyzkousene = new Dictionary<((int, int), (int, int)), int>();
+            (int,int)[] tahy = {(1,0),(-1,0),(0,1),(0,-1)};
+            vyzkousene.Add((skladnik, bedna), 0);
+            int kroky = 0;
+            int zacatek_fronty = -1;
+            int konec_fronty = 0;
+            while (bedna != cil && bedna != (0,0))
+            {
+                zacatek_fronty++;
+                foreach (var tah in tahy)
+                {
+                    int muzu = MuzuTahnout(skladiste, skladnik, bedna, tah, cil);
+                    if (muzu == 0)
+                    {
+                        if (! vyzkousene.ContainsKey(((skladnik.Item1 + tah.Item1, skladnik.Item2 + tah.Item2), bedna)))
+                        {
+                            vyzkousene.Add(((skladnik.Item1 + tah.Item1, skladnik.Item2 + tah.Item2), bedna),0);
+                            fronta[konec_fronty] = ((skladnik.Item1 + tah.Item1, skladnik.Item2 + tah.Item2), bedna,kroky + 1);
+                            konec_fronty++;
+                        }
+                    }
+                    else if (muzu == 1)
+                    {
+                        if (! vyzkousene.ContainsKey(((skladnik.Item1 + tah.Item1, skladnik.Item2 + tah.Item2), (bedna.Item1 + tah.Item1, bedna.Item2 + tah.Item2))))
+                        {
+                            vyzkousene.Add(((skladnik.Item1 + tah.Item1, skladnik.Item2 + tah.Item2), (bedna.Item1 + tah.Item1, bedna.Item2 + tah.Item2)),0);
+                            fronta[konec_fronty] = ((skladnik.Item1 + tah.Item1, skladnik.Item2 + tah.Item2), (bedna.Item1 + tah.Item1, bedna.Item2 + tah.Item2),kroky + 1);
+                            konec_fronty++;
+                        }
+                    }
+                }    
+                skladnik = fronta[zacatek_fronty].Item1;
+                bedna = fronta[zacatek_fronty].Item2;
+                kroky = fronta[zacatek_fronty].Item3;
+            }
+            if (bedna == (0,0))
+            {
+                Console.WriteLine(-1);
+            }
+            else
+            {
+                Console.WriteLine(kroky);
+            }
+        }
+        static void Main(string[] args)
+        {
+            int[,] skladiste = VyrobSkladiste();
+            List<(int, int)> polozky = VyplnSkladiste(skladiste);
+            (int, int) skladnik = polozky[0];
+            (int, int) bedna = polozky[1];
+            (int, int) cil = polozky[2];
+            Hledej(skladiste, skladnik, bedna, cil);
+        }
+    }
+}
---
+++ C# - projekty/TextJustifier/DU_4/Program.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace DU_3
+{
+    /* Programování v C#
+    * DÚ 4
+    * Multiple text justification
+    */
+    internal class Program
+    {
+        // class that catches error and reacts to it
+        static class ExeptionHandler
+        {
+            // function for catching argument errors
+            static public bool ArgErr(string[] args)
+            {
+                if (args.Length < 3 || (args[0] == "--highlight-spaces" && args.Length < 4))
+                {
+                    Console.WriteLine("Argument Error");
+                    return true;
+                }
+                // check if the third argument is positive integer or zero
+                try
+                {
+                    if (Convert.ToInt32(args[args.Length - 1]) < 0)
+                    {
+                        Console.WriteLine("Argument Error");
+                    }
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Argument Error");
+                    return true;
+                }
+                return false;
+            }
+            static public LineProcessor OutputErr(string[] args)
+            {
+                try
+                {
+                    LineProcessor p = new LineProcessor(new System.IO.StreamWriter(args[args.Length - 2]), Convert.ToInt32(args[args.Length - 1]));
+                    return p;
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("File Error");
+                    return null;
+                }
+            }
+        }
+        // Class that prepares line for output and than prints it to outputfile
+        class LineProcessor
+        {
+            public System.IO.StreamWriter writer;
+            private int chars_to_line; // line size in chars
+            private string line = "";
+            private char wordSeparator = ' ';
+            private string lineEnd = "\n";
+            private bool emptyOut = true;
+            public bool new_paragraph = false; // true if the sentence which is going to be print should be in a next paragraph
+
+            public LineProcessor(StreamWriter writer, int chars_to_line)
+            {
+                this.writer = writer;
+                this.chars_to_line = chars_to_line;
+            }
+
+            public void HighlightSpaces()
+            {
+                wordSeparator = '.';
+                lineEnd = "<-\n";
+            }
+            // function that takes word from reader and prepares one line for output
+            public void AddWord(string word)
+            {
+                emptyOut = false;
+                if (chars_to_line >= line.Length + word.Length + 1) // test if the word could be appended to actual line
+                {
+                    if (line.Length != 0)
+                        line += ' ';
+                    line += word;
+                }
+                else
+                {
+                    if (line.Length != 0)
+                        ProcessLine();
+                    line = word;
+                }
+            }
+            // function that fills up line with spaces and prints it to output file
+            private void ProcessLine()
+            {
+                if (new_paragraph)
+                {
+                    // indent new paragraph
+                    writer.Write(lineEnd);
+                }
+                int to_fill = chars_to_line - line.Length;
+                string[] words = line.Split(' ');
+                if (words.Length > 1) // test if line is not a silgle word
+                {
+                    int spaces = to_fill / (words.Length - 1) + 1;
+                    for (int i = 0; i < words.Length - 1; i++)
+                    {
+                        writer.Write(words[i]);
+                        if (to_fill % (words.Length - 1) > i)
+                            writer.Write(new String(wordSeparator, spaces + 1));
+                        else
+                            writer.Write(new String(wordSeparator, spaces));
+                    }
+                }
+                writer.Write(words[words.Length - 1] + lineEnd);
+                new_paragraph = false;
+            }
+            // function that formate and prints to output file last line of paragraph
+            public void EndOfParagraph()
+            {
+                if (line.Length != 0 || (!new_paragraph && emptyOut))
+                    WriteLine();
+                line = "";
+            }
+            private void WriteLine()
+            {
+                string[] words = line.Split(' ');
+                for (int i = 0; i < words.Length - 1; i++)
+                {
+                    writer.Write(words[i] + wordSeparator);
+                }
+                writer.Write(words[words.Length - 1] + lineEnd);
+            }
+        }
+        // class that reads whole input file and passes words to LineProcessor
+        class WordReader
+        {
+            private char[] white_space_chars = { '\n', ' ', '\t' };
+            public StreamReader reader;
+            private string current_word = "";
+            private bool empty_line = true; // true if first chars of input line are white space chars
+
+            public WordReader(StreamReader sr)
+            {
+                this.reader = sr;
+            }
+            public bool IsWhiteSpace(char ch)
+            {
+                return Array.Exists(white_space_chars, x => x == ch);
+            }
+            // function that reads input file char by char and passes words to LineProcessor
+            public void ReadFile(LineProcessor p)
+            {
+                char ch;
+                while (((ch = (char)reader.Read()) + 65536) % 65536 != 65535)
+                {
+                    if (!IsWhiteSpace(ch))
+                    {
+                        current_word += ch; // append char to actual word
+                        empty_line = false;
+                    }
+                    else
+                    {
+                        if (ch == '\n')
+                        {
+                            if (empty_line && !p.new_paragraph) //test if the line is full of white space chars and is the first one of this lines
+                            {
+                                p.new_paragraph = true;
+                                p.EndOfParagraph();
+                                current_word = "";
+                            }
+                            empty_line = true;
+                        }
+                        // pass word to the LineProcessor and clears it
+                        if (current_word.Length != 0)
+                            p.AddWord(current_word);
+                        current_word = "";
+                    }
+                }
+                p.new_paragraph = false;
+            }
+        }
+        private static void Main(string[] args)
+        {
+            int firstInputIndex = 0;
+            LineProcessor p;
+            if (!ExeptionHandler.ArgErr(args) && (p = ExeptionHandler.OutputErr(args)) != null)
+            { 
+                if (args[0] == "--highlight-spaces")
+                {
+                    firstInputIndex = 1;
+                    p.HighlightSpaces();
+                }
+                WordReader wr;
+                using (p.writer)
+                {
+                    for (int i = firstInputIndex; i < args.Length - 2; i++)
+                    {
+                        try
+                        {
+                            wr = new WordReader(new StreamReader(args[i]));
+                            wr.reader = new StreamReader(args[i]);
+                            wr.ReadFile(p);
+                        }
+                        catch (IOException)
+                        {
+                            continue;
+                        }
+                    }
+                    p.EndOfParagraph();
+                }
+                
+            }
+        }
+    }
+}
